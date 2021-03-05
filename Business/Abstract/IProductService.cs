dfcf565cab50800a1d544c983c469ff1b67808d5
Arzu Ünüvar <62670222+arzunuvar@users.noreply.github.com>
2021-03-05 16:08:53 +0300

    AOP ve JWT

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -22,5 +22,9 @@ namespace Business.Abstract
         IResult Add(Product product);
 
         IResult Update(Product product);
+
+        IResult AddTransactionalTest(Product product);
+
+        //RESTFUL -->HTTP -->
     }
 }
